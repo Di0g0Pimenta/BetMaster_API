9ecaa2cd0341619164f949366c0b81311b9d5ae4
Diogo Pimenta <106586929+Di0g0Pimenta@users.noreply.github.com>
2023-11-30 00:44:15 +0000

    Update routes/bets.js

--- src/routes/bets.js
+++ src/routes/bets.js
@@ -22,14 +22,14 @@ const verifyOddExists = async (oddId) => {
 
 const verifyUserBalance = async (userId, betAmount) => {
   try {
-    console.log('Verificando saldo do usuário. UserID:', userId);
+    console.log("Verificando saldo do usuário. UserID:", userId);
 
     const user = await User.findById(userId);
 
-    console.log('User encontrado:', user);
+    console.log("User encontrado:", user);
 
     if (!user || !user.balance || user.balance < betAmount) {
-      console.log('Saldo insuficiente. User ou balance inválido.');
+      console.log("Saldo insuficiente. User ou balance inválido.");
 
       throw "Saldo insuficiente para a aposta.";
     }
@@ -39,7 +39,6 @@ const verifyUserBalance = async (userId, betAmount) => {
     throw `Erro ao verificar o saldo do usuário: ${error}`;
   }
 };
-
 
 router.get("/", async (req, res) => {
   try {
@@ -53,15 +52,15 @@ router.get("/", async (req, res) => {
         select: "teamAOdd teamBOdd drawOdd",
       });
 
-      const formattedBets = bets.map((bet) => ({
-        betId: bet._id ? bet._id.toString() : null,
-        gameId: bet.gameId,
-        oddId: bet.oddId,
-        betAmount: bet.betAmount,
-        betType: bet.betType,
-        status: bet.status,
-        dateTime: bet.dateTime,
-      }));
+    const formattedBets = bets.map((bet) => ({
+      betId: bet._id ? bet._id.toString() : null,
+      gameId: bet.gameId,
+      oddId: bet.oddId,
+      betAmount: bet.betAmount,
+      betType: bet.betType,
+      status: bet.status,
+      dateTime: bet.dateTime,
+    }));
 
     res.status(200).json(formattedBets);
   } catch (error) {
@@ -73,11 +72,11 @@ router.post("/", async (req, res) => {
   try {
     const { gameId, oddId, betAmount, betType } = req.body;
 
-    console.log('User object in create bet route:', req.user);
-    console.log('OddId in create bet route:', oddId);
+    console.log("User object in create bet route:", req.user);
+    console.log("OddId in create bet route:", oddId);
 
     if (!req.user || !req.user.userId) {
-      console.log('User ID not defined. Sending error response.');
+      console.log("User ID not defined. Sending error response.");
       return handleError(
         res,
         500,
@@ -87,12 +86,12 @@ router.post("/", async (req, res) => {
 
     // Verificar se a Odd existe
     const odd = await Odd.findById(oddId);
-    console.log('Response from database:', odd);
+    console.log("Response from database:", odd);
 
-    console.log('Odd found:', odd);
+    console.log("Odd found:", odd);
 
     if (!odd) {
-      console.log('Odd not found. Sending error response.');
+      console.log("Odd not found. Sending error response.");
       return handleError(
         res,
         404,
@@ -104,8 +103,18 @@ router.post("/", async (req, res) => {
     await verifyOddExists(oddId);
     await verifyUserBalance(req.user.userId, betAmount);
 
-    const userId = req.user && req.userId.toString();
-    console.log('UserID ao criar aposta:', userId);
+    const userId =
+      req.user && req.user.userId ? req.user.userId.toString() : null;
+    console.log("UserID ao criar aposta:", userId);
+
+    if (!userId) {
+      console.log("User ID not defined. Sending error response.");
+      return handleError(
+        res,
+        500,
+        "Erro ao criar a aposta: ID do usuário não está definido."
+      );
+    }
 
     const newBet = new Bet({
       userId,
@@ -117,22 +126,38 @@ router.post("/", async (req, res) => {
       dateTime: new Date(),
     });
 
-    console.log('Objeto da Nova Aposta:', newBet);
+    console.log("Objeto da Nova Aposta:", newBet);
 
     await newBet.save();
 
-    const user = await User.findById(req.userId);
+    // Recarrega o objeto newBet para obter a versão atualizada do documento
+    const newBetAfterSave = await Bet.findById(newBet._id);
+
+    console.log("Objeto da Nova Aposta após salvar:", newBetAfterSave);
+
+    if (!newBetAfterSave) {
+      console.error("Erro: A aposta não foi salva corretamente.");
+      return res.status(500).json({ message: "Erro ao criar a aposta." });
+    }
+
+    const user = await User.findById(userId);
+
+    console.log("User encontrado após a busca:", user);
+
+    if (!user || user.balance === undefined) {
+      console.log("Usuário não encontrado ou saldo não definido.");
+      throw new Error("Erro ao obter informações do usuário.");
+    }
+
     const userBalance = user.balance;
     const updatedBalance = userBalance - betAmount;
+
     await User.findByIdAndUpdate(req.userId, { balance: updatedBalance });
 
-
-    res
-      .status(201)
-      .json({
-        message: "Aposta criada com sucesso.",
-        betId: newBet._id.toString(),
-      });
+    res.status(201).json({
+      message: "Aposta criada com sucesso.",
+      betId: newBetAfterSave._id ? newBetAfterSave._id.toString() : null,
+    });
   } catch (error) {
     handleError(res, 500, `Erro ao criar a aposta: ${error}`);
   }
